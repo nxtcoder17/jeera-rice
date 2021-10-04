ce8785573e9b8de04abf7bce4fee4b6caa397298
nxtcoder17 <nxtcoder17@gmail.com>
2021-10-04 13:57:32 +0530

    added script to use ffmpeg to record screen

--- bin/record-screen.sh
+++ bin/record-screen.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/env sh
-ffmpeg -f x11grab -video_size 1920x1080 -framerate 25 -i $DISPLAY -c:v ffvhuff "$1"
+
+ffmpeg -f x11grab -video_size 3840x2160  -framerate 25 -i $DISPLAY -c:v ffvhuff "$1".mkv
