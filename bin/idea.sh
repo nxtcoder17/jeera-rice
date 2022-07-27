1367ed542ef3ad17cec10c3fb6510a687a41b1ad
nxtcoder17 <nxtcoder17@gmail.com>
2022-07-27 09:23:22 +0530

    added nxtcoder17 userstyle

--- bin/idea.sh
+++ bin/idea.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
 # CMD="systemd-run --user --slice=idea.slice $IDEA_DIR/bin/idea.sh"
-CMD="$HOME/.local/tars.uz/idea-IU-221.5921.22/bin/idea.sh"
-# CMD="/home/nxtcoder17/.local/tars.uz/idea-IU-222.3244.4/bin/idea.sh"
+# CMD="$HOME/.local/tars.uz/idea-IU-221.5921.22/bin/idea.sh"
+CMD="$HOME/.local/tars.uz/idea-IU-222.3345.118/bin/idea.sh"
 
 [ -f $CMD ] || (echo "executable CMD ($CMD) does not exist" && exit 1)
 
