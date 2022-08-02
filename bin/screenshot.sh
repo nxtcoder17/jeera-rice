d856c9e16f11c96cecfb44576a31e127f7070cea
nxtcoder17 <nxtcoder17@gmail.com>
2022-08-02 18:32:54 +0530

    added screenshot, updated ideavimrc and i3config for rofi fuzzy find

---
+++ bin/screenshot.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+name=""
+if [ -z "$1" ]; then
+  name="/tmp/screenshot-$RANDOM".png
+else
+  dir="$HOME/pictures/screenshots"
+  mkdir -p $dir
+  name="$dir/$1"
+fi
+
+scrot -s $name
+
+dragon-drag-and-drop $name
