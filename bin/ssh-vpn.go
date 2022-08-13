6965e842d3abf191923fba300aebec04e5c36992
nxtcoder17 <nxtcoder17@gmail.com>
2022-08-14 00:40:42 +0530

    ssh-vpn finally works

--- bin/ssh-vpn.go
+++ bin/ssh-vpn.go
@@ -5,39 +5,42 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func main() {
-	var namespace string
-	var name string
-	flag.StringVar(&namespace, "namespace", "", "--namespace")
-	flag.StringVar(&name, "name", "shuttle-proxy", "--name")
-	flag.Parse()
-
-	if namespace == "" || name == "" {
-		panic("must specify --namespace and --name")
+	splits := strings.Split(os.Args[1], "/")
+	if len(splits) != 2 {
+		panic("first argument must be a namespaced-name in format <namespace>/<name>")
 	}
 
-	// fmt.Println("namespace: ", namespace)
-	// fmt.Println(flag.Args())
+	namespace := splits[0]
+	name := splits[1]
+
+	// fmt.Println(os.Args)
+
+	ipRanges := make([]string, 0, len(os.Args))
+	if len(os.Args)> 2 {
+		ipRanges = os.Args[2:]
+	} else {
+		ipRanges = append(ipRanges, "0.0.0.0/0")
+	}
 
 	runServerProxy := fmt.Sprintf("kubectl run %s --namespace %s --image=nxtcoder17/alpine.python3:nonroot --restart=Never -- sh -c 'tail -f /dev/null'", name, namespace)
-
-	kCmd := exec.Command("bash", "-c", runServerProxy)
-	kCmd.Stderr = os.Stderr
-	kCmd.Stdout = os.Stdout
-	kCmd.Stdin = os.Stdin
-	fmt.Printf("kubectl output: %v\n", kCmd.Run())
+	b, err := exec.Command("bash", "-c", runServerProxy).CombinedOutput()
+	fmt.Printf("(kubectl): %s", b)
+	if err != nil {
+		fmt.Printf("trying to use existing pod, in case it already exists\n")
+		// fmt.Println("err occurred while creating pod:", err)
+	}
 
 	args := make([]string, 0, len(flag.Args()) * 2)
-	// args = append(args, "-c", "sshuttle", "--dns", "-r", name, "-e", "ssh-vpn.go-khelper.sh")
-	args = append(args, "-c", fmt.Sprintf("sshuttle --dns -r %s -e ssh-vpn.go-khelper.sh", name))
-	args = append(args, flag.Args()...)
-	fmt.Printf("args: %v\n", args)
-	c := exec.Command("bash", args...)
+	args = append(args, "--dns", "-r", fmt.Sprintf("%s/%s", namespace, name), "-e", "ssh-vpn.go-khelper.sh")
+	args = append(args, ipRanges...)
+	c := exec.Command("sshuttle", args...)
 	c.Stdin = os.Stdin
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
-	fmt.Println(c.Run())
+	c.Run()
 }
 
--- bin/ssh-vpn.go-khelper.sh
+++ bin/ssh-vpn.go-khelper.sh
@@ -1,9 +1,26 @@
+#! /usr/bin/env bash
+
 echo "$@" > /tmp/ol
-podName=$1
+podRef=$1
+IFS=/; read -a items <<< $podRef
+
+if [ ${#items} -gt 1 ]; then
+  namespace=${items[0]}
+  podName=${items[1]}
+else
+  podName=${items[0]}
+fi
+
 echo "$1" >> /tmp/ol
 shift 2;
 echo "$@" >> /tmp/ol
 
-echo exec kubectl exec -i $podName -- "$@" >> /tmp/ol
-exec kubectl exec -i $podName -- $@
+if [ $namespace != "" ]; then
+  echo "exec kubectl exec -n $namespace -i $podName -- $@" >> /tmp/ol
+  eval kubectl exec -n $namespace -i $podName -- "$@"
+else
+  echo "exec kubectl exec -i $podName -- $@" >> /tmp/ol
+  eval kubectl exec -i $podName -- "$@"
+fi
+# exec kubectl exec -i $podName -- $@
 
