4ee7d98c73b0f82575e83d5e43b71bc51df6a4b3
nxtcoder17 <nxtcoder17@gmail.com>
2025-06-02 09:19:30 +0530

    feat: kitty, and tmux updates

--- apps/tmux/.config/tmux/scripts/choose-tmux-session.sh
+++ apps/tmux/.config/tmux/scripts/choose-tmux-session.sh
@@ -1,12 +1,6 @@
-#! /usr/bin/env bash
-
-# tmux list-sessions -F '#S' | grep -v \"^$(tmux display-message -p '#S')\$\" | fzf --reverse --prompt 'choose session ❭❭ ' | xargs -r tmux switch-client -t
-# echo "$FZF_DEFAULT_OPTS"
+#!/usr/bin/env bash
 
 current_session=$(tmux display-message -p '#S')
-items=$(tmux list-sessions -F '#S' | grep -v "$current_session")
+items=$(tmux list-sessions -F '#S' | grep -v "$current_session\$")
 
-# items+="\n$(tmux display-message -p '#{pane_id}')"
-
-# tmux list-sessions -F '#S' | grep -v \"^$(tmux display-message -p '#S')\$\" | fzf --reverse --prompt 'choose session ❭❭ ' | xargs -r tmux switch-client -t
 echo "$items" | fzf --reverse --prompt 'choose session ❭❭ ' | xargs -r tmux switch-client -t
--- apps/tmux/.config/tmux/themes/nxtcoder17/dark.sh
+++ apps/tmux/.config/tmux/themes/nxtcoder17/dark.sh
@@ -1,8 +1,10 @@
 # vim: set ft=bash:
-hl_bg="#325158"
-hl_fg="#789cbf"
-hl_selected_bg="#63b8c7"
-hl_selected_fg="#395257"
+# hl_bg="#325158"
+# hl_fg="#789cbf"
+# hl_selected_bg="#63b8c7"
+# hl_selected_fg="#395257"
+#
+source ~/.base16/tmux-base16.sh
 
 # [glymphs](https://www.compart.com/en/unicode/category/So)
 # ░ \u2591 ░ \u2592 ▓ \u2593
@@ -26,8 +28,8 @@ set-window-option -g clock-mode-colour "$hl_selected_bg"
 set-window-option -g clock-mode-style 24
 
 # pane
-set-window-option -g pane-active-border-style fg="$hl_selected_bg",bold
-set-window-option -g pane-border-style fg="$hl_bg"
+set-window-option -g pane-active-border-style fg="$hl_selected_fg",bold
+set-window-option -g pane-border-style fg="$hl_fg"
 # set-window-option -g pane-border-lines "single"
 set -g pane-border-status off
 
@@ -47,13 +49,13 @@ set -g @tmux-pop-color black
 # status-bar
 set -g window-status-format "#[fg=$hl_bg,bg=default]$icon_window_left#[fg=$hl_fg,bg=$hl_bg] #I#[fg=$hl_bg,bg=default]$icon_num_separator#[fg=$hl_fg,bg=default] #W #{?pane_synchornized,$synchronized_icon, #F} #[fg=$hl_fg]$icon_window_right"
 
-set -g window-status-current-format "#[fg=$hl_bg,bg=default]$icon_window_left#[fg=$hl_selected_fg,bg=$hl_selected_bg] #I#[fg=$hl_selected_bg,bg=default]$icon_num_separator#[fg=$hl_selected_bg,bg=default] #W #{?pane_synchronized,$synchronized_icon,#F} #[fg=$hl_fg]$icon_window_right"
+set -g window-status-current-format "#[fg=$hl_bg,bg=default]$icon_window_left#[bold]#[fg=$hl_selected_fg,bg=$hl_selected_bg] #I#[fg=$hl_selected_bg,bg=default]$icon_num_separator#[fg=$hl_selected_fg,bg=default] #W #{?pane_synchronized,$synchronized_icon,#F} #[fg=$hl_fg]$icon_window_right"
 
 set -g status-left ' 🌳 '
 
 # set -g status-right "#{prefix_highlight} #[fg=$hl_bg]$icon_hostname_left#[fg=$hl_selected_bg,bg=$hl_bg]   #S (#H) #[fg=$hl_bg,bg=default,nobold,noitalics,nounderscore]$icon_hostname_right#[bg=default] "
 
-set -g status-right "#{prefix_highlight} #[fg=$hl_selected_bg,bg=default]   #S (#H) #[fg=$hl_selected_bg,bg=default,nobold,noitalics,nounderscore]#[bg=default] "
+set -g status-right "#{prefix_highlight} #[fg=$hl_selected_fg,bg=default]   #S (#H) #[fg=$hl_selected_bg,bg=default,nobold,noitalics,nounderscore]#[bg=default] "
 
 # plugin: prefix-hightlight
 set -g @prefix_highlight_output_prefix "#[fg=$hl_bg]#[bg=default]#[nobold]#[noitalics]$icon_hostname_left#[fg=$hl_selected_bg]#[bg=$hl_bg]"
